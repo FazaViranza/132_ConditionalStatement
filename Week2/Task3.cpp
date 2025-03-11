e80c8bc66cbb2e0847167ff7890fc304c741519f
FazaViranza <nofila.faza.ft24@mail.umy.ac.id>
2025-03-11 20:43:48 +0700

    Update Task3.cpp

--- Week2/Task3.cpp
+++ Week2/Task3.cpp
@@ -1,27 +1,26 @@
-#include <iostream>
+#include <iostream> 
 using namespace std;
 
-float FungsiHitungRerata(float p, float q){
-    return (p + q) / 2;
+float FungsiHitungLuasRerata(float x, float y){
+    return (x * y) / 2;
 }
 
-string FungsiCekStatus(float r){
-    if (r >=60){
-        return "Lulus";
-    }
-    else{
-        return "Tidak Lulus";
+string FungsiCekStatus(float a){
+    if (a >= 60){
+        return "lulus";
+    } else {
+        return "tidak lulus";
     }
 }
 
-int main(){
-    float Nilai 1, Nilai2;
+int main()
+{
+    float Nilai1, Nilai2;
 
-    cout << "Masukkan Nilai 1 : ";
+    cout << "Masukkan Nilai 1: ";
     cin >> Nilai1;
-    cout << "Masukkan Nilai 2 : ";
+    cout << "Masukkan Nilai 2: ";
     cin >> Nilai2;
-
-    cout << "Status Kelulusan : "
-    << FungsiCekStatus(FungsiHitungRerata(Nilai1, Nilai2));
+    cout << "Status Kelulusan: " 
+    << FungsiCekStatus(FungsiHitungLuasRerata(Nilai1, Nilai2));
 }
